ea078e2447d009a51fea72831f57f843f6a6f537
dilowagner <diegowagner4@gmail.com>
2017-02-16 22:43:15 -0200

    Update IOS project

--- iOS/DatabaseConnection.cs
+++ iOS/DatabaseConnection.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.IO;
+using DscFoods.Infrastructure;
+using SQLite.Net;
+using DscFoods.iOS;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseConnection))]
 namespace DscFoods.iOS
 {
-	public class DatabaseConnection
+	public class DatabaseConnection : IDatabaseConnection
 	{
-		public DatabaseConnection()
+		public SQLiteConnection DbConnection()
 		{
+			var dbName = "DscFoodsDb.db3";
+			string personalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string libraryFolder = Path.Combine(personalFolder, "..", "Library");
+			var path = Path.Combine(libraryFolder, dbName);
+			return new SQLiteConnection(path);
 		}
 	}
 }
